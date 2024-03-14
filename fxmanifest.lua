134479fbe27f49715e437bcda6c7e5a19285cafa
BestyFir3TM <55640441+BestyFir3TM@users.noreply.github.com>
2024-03-14 01:26:19 +0100

    Default Files

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'adamant'
+game 'gta5'
+lua54 'yes'
+
+Author ''
+Description ''
+Version ''
+
+server_scripts {
+    'Config/Config.logs.lua',
+    'Config/Config.lua',
+    'Server/*.lua'
+}
+
+client_scripts {
+    'Config/Config.lua',
+    'Client/*.lua'
+}
